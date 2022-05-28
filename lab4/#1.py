0e106de4764542e13a03254c05484247f83d8e0b
Rodion Voronin <vorongenius@gmail.com>
2022-05-28 11:19:58 +0500

    Create #1.py

---
+++ lab4/#1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 100)
+y = x**2 - x**5 + 1
+plt.plot(x, y, 'r-', label='Функция 1')
+y1 = x**3 + x**7
+plt.plot(x, y1, 'b-', label='Функция 2')
+
+plt.legend()
+plt.show()
